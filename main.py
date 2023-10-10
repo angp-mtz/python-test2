0f170050609b284620e88565c31c9a9c8d5f2792
angp-mtz <123033185+angp-mtz@users.noreply.github.com>
2023-10-10 14:00:28 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("today is a good day to learn Python")
+print("Python is fun")
+# fixed the quotation mark
+print("Python's strings are easy to use")
+# fixed the quotation mark and added an 's' after string
+print('We can even included "quotes" in strings')
+# fixed the quotation mark
+print("hello" + "world")
+print("hello" + ' ' + "world")
+greeting = "hello"
+# fixed the quotation mark
+name = input("please enter you name: ")
+print(greeting + name)
+# if we want a space, we can do that too
+print(greeting + ' ' + name)
